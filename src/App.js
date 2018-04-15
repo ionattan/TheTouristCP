1bcc790903c5b23ca354d8c2959e334b3e373ddd
ionattan <imendez@geeksolutions.mx>
2018-04-15 04:00:43 +0000

    [REACT] Installing react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import MenuBar from "./MenuBar/MenuBar";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <MenuBar/>
+          <h2>asda</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/MenuBar/MenuBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class MenuBar extends Component {
+    render() {
+        return (
+            <div className="MenuBar">
+                <h1>MenuBar Starter</h1>
+            </div>
+        );
+    }
+}
+
+export default MenuBar;
